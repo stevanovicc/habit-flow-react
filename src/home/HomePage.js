09076e7683023948fbc96d91be3ebe7a357aa3b2
Filip Stevanovic <fstevanovic2003@gmail.com>
2025-01-08 19:40:22 +0100

    Added a welcome message to the Home Page that fetches the current user's first and last name from firebase

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import {useEffect,useState} from 'react';
+import { getAuth,onAuthStateChanged } from 'firebase/auth';
+import { getFirestore,doc,getDoc } from 'firebase/firestore';
 
 const HomePage = () => {
-    return(
-        <h1>Welcome to HabitFlow!</h1>
-    )
+    const [userName,setUserName] = useState('');
+    const auth = getAuth();
+    const db = getFirestore();
+
+    useEffect(() => {
+        const fetchUserData = async (user) =>{
+            try{
+                const userDoc= await getDoc(doc(db,"users", user.uid));
+                if(userDoc.exists()){
+                    const data = userDoc.data();
+                    setUserName(`${data.firstName} ${data.lastName}`);
+                }
+            } catch(error){
+                console.error("Error fetching user data", error);
+            }
+        };
+        const unsubscribe = onAuthStateChanged(auth, (user) =>{
+            if (user){
+                fetchUserData(user);
+            }
+        });
+        return unsubscribe;
+    }, [auth,db]);
+    return <h1>Welcome to HabitFlow, {userName}!</h1>
 }
 export default HomePage;
