f1ef16c19c70b92bce9fcbd8844ad9465bbf926f
Filip Stevanovic <fstevanovic2003@gmail.com>
2025-01-08 21:51:28 +0100

    Added an add habit button that shows a form you have to fill it includes: habit name, habit description and an habit frequency option (everyday, once a week, once a month)

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import {useEffect,useState} from 'react';
 import { getAuth,onAuthStateChanged } from 'firebase/auth';
-import { getFirestore,doc,getDoc } from 'firebase/firestore';
+import { getFirestore,doc,getDoc,collection,addDoc } from 'firebase/firestore';
+import "./HomePage.css"
 
 const HomePage = () => {
     const [userName,setUserName] = useState('');
     const auth = getAuth();
     const db = getFirestore();
+    const [showHabitform, setShowHabitForm] = useState(false);
+    const [habitName,setHabitName] = useState("");
+    const [habitDescription, setHabitDescription] = useState("");
+    const [habitFrequency, setHabitFrequency] = useState("everyday");
+    const [successMessage, setSuccessMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const handleAddHabitClick = () => {
+        setShowHabitForm(true);
+    }
+
+    const handleSubmit = async () => {
+        try{
+            const habitData = {
+                name: habitName,
+                description: habitDescription,
+                frequency: habitFrequency,
+            };
+
+            await addDoc(collection(db,"habits"), habitData);
+            setHabitName("");
+            setHabitDescription("");
+            setHabitFrequency("everyday");
+            setShowHabitForm(false);
+
+            setSuccessMessage("Habit added successfully!");
+            setTimeout(() => setSuccessMessage(""), 3000);
+        }catch(error){
+            setErrorMessage("Failed to add habit. Please try again.");
+            setTimeout(() => setErrorMessage(""), 3000);
+            console.error("Error adding habit: ", error);
+        }
+
+    };
+
+    const handleCancel = () => {
+        setShowHabitForm(false);
+    }
 
     useEffect(() => {
         const fetchUserData = async (user) =>{
@@ -27,6 +66,56 @@ const HomePage = () => {
         });
         return unsubscribe;
     }, [auth,db]);
-    return <h1>Welcome to HabitFlow, {userName}!</h1>
-}
+    return (
+    <div className='home-container'>
+        <h1>Welcome to HabitFlow, {userName}!</h1>
+        {!showHabitform &&(
+        <button onClick={handleAddHabitClick} className='habit-button'>Add Habit</button>
+        )}
+        {successMessage && <div className='success-message'>{successMessage}</div>}
+        {errorMessage && <div className='error-message'>{errorMessage}</div>}
+
+        {showHabitform &&(
+            <div className="habit-form">
+                <h2 className='habit-h2'>Add a New Habit</h2>
+                <label className='habit-label'>
+                    Habit Name:
+                    <input
+                    type='text'
+                    value={habitName}
+                    onChange={(e) => setHabitName(e.target.value)}
+                    className='habit-input'
+                    />
+                </label>
+                <br/>
+                <label className='habit-label'>
+                    Description:
+                    <input
+                    type='text'
+                    value={habitDescription}
+                    onChange={(e) => setHabitDescription(e.target.value)}
+                    className='habit-input'
+                    />
+                </label>
+                <br/>
+                <label className='habit-label'>
+                    Frequency
+                    <select
+                    value={habitFrequency}
+                    onChange={(e) => setHabitFrequency(e.target.value)}
+                    className='habit-select'
+                    >
+                    <option value="everyday">Everyday</option> {/*TODO try with enum */}
+                    <option value="once-a-week">Once a week</option>
+                    <option value="once-a-month">Once a month</option>
+                    </select>
+                </label>
+                <br/>
+                <button onClick={handleSubmit} className='habit-button-form'>Save</button>
+                <button onClick={handleCancel} className='habit-button-form'>Cancel</button>
+            </div>
+        )}
+    </div>
+    );
+};
 export default HomePage;
